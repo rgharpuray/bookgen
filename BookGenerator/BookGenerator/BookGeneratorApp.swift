159897fa0c00e7015105c97fb7049cb9908ee510
Rishi <rishi@Rishis-MacBook-Pro-2.local>
2025-09-23 18:03:09 -0700

    bookgen

---
+++ BookGenerator/BookGenerator/BookGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookGeneratorApp.swift
+//  BookGenerator
+//
+//  Created by Rishi on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BookGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookGenerator/BookGeneratorTests/BookGeneratorTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookGeneratorTests.swift
+//  BookGeneratorTests
+//
+//  Created by Rishi on 9/23/25.
+//
+
+import Testing
+
+struct BookGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
